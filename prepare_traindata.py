8d386d2c0d69f1924924b70bb14c6bebe100e6f5
Ananthoju Pranav Sai <80518918+Ananthoju-Pranav-Sai@users.noreply.github.com>
2022-08-12 12:39:22 +0530

    Changed files

--- prepare_traindata.py
+++ prepare_traindata.py
@@ -5,21 +5,31 @@ import time
 import os
 from getkeys import key_check
 
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    vertices = np.array([[10,600],[10,300], [310,280], [500,280], [800,300], [800,600]], np.int32)
+    processed_img = roi(processed_img, [vertices])
+    processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=150)
+    processed_img = processed_img[280:600][:]
+    return processed_img
+
 # creating one hot encoded label for every key
-w = [1,0,0,0,0,0,0,0,0]
-s = [0,1,0,0,0,0,0,0,0]
-a = [0,0,1,0,0,0,0,0,0]
-d = [0,0,0,1,0,0,0,0,0]
-wa = [0,0,0,0,1,0,0,0,0]
-wd = [0,0,0,0,0,1,0,0,0]
-sa = [0,0,0,0,0,0,1,0,0]
-sd = [0,0,0,0,0,0,0,1,0]
-nk = [0,0,0,0,0,0,0,0,1]    # this represents no key is being pressed
+w = 0
+s = 1
+a = 2
+d = 3
+nk = 4    # this represents no key is being pressed
 
 start_value = 1
 
 while True:
-    file_name = "train_data-{}.npy".format(start_value)
+    file_name = "fpp_training_data/train_data-{}.npy".format(start_value)
 
     if (os.path.isfile(file_name)):
         print('File exists, proceeding further ',start_value)
@@ -28,26 +38,15 @@ while True:
         print('File doesnot exist, starting fresh! ',start_value)
         break
 def keys_to_outputs(keys):
-    output = [0,0,0,0,0,0,0,0]
-    if ('W' in keys and 'A' in keys):
-        output = wa
-    elif ('W' in keys and 'D' in keys):
-        output = wd
-    elif ('S' in keys and 'A' in keys):
-        output = sa
-    elif ('S' in keys and 'D' in keys):
-        output = sd
-    elif ('W' in keys):
-        output = w
-    elif ('S' in keys):
-        output = s
-    elif ('A' in keys):
-        output = a
+    if ('A' in keys):
+        return a
     elif ('D' in keys):
-        output = d
-    else:
-        output = nk
-    return output
+        return d
+    elif ('W' in keys):
+        return w
+    elif ('S' in keys):
+        return s
+    return nk
 
 def main(file_name,start_value):
     train_data = []
@@ -58,27 +57,36 @@ def main(file_name,start_value):
     
     paused = False
     print('Starting captures')
+    size = 100
+    count = np.zeros(5)
+    n = 0
     while(True):
         if not paused:
             screen = grab_screen(region=(10,40,800,600))
-
-            screen = cv2.resize(screen,(480,270))
-            screen = cv2.cvtColor(screen,cv2.COLOR_BGR2RGB)
+            screen = process_img(screen)
 
             keys = key_check()
-            print(keys)
-            output = keys_to_outputs(keys)
-            train_data.append([screen,output])
-
-            if(len(train_data)%100 == 0):
-                print(len(train_data))
-
-                if(len(train_data) == 500):
-                    np.save(file_name,train_data)
-                    print('Finished saving')
-                    train_data = []
-                    start_value +=1
-                    file_name = f"train_data-{start_value}.npy"
+            # print(keys)
+            output = [0,0,0,0,0]
+            code = keys_to_outputs(keys)
+            output[code]=1
+            if(count[code]!=size):
+                train_data.append([screen,output])
+                count[code]+=1
+                n+=1
+            else:
+                continue
+            
+            print(n,size*5)
+            print(count)
+            if(n==size*5):
+                np.save(file_name,train_data)
+                print('Finished saving')
+                train_data = []
+                start_value +=1
+                file_name = f"fpp_training_data/train_data-{start_value}.npy"
+                count = np.zeros(5)
+                n = 0
     
         keys = key_check()
         if 'P' in keys:
